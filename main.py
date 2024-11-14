bec6c93f586c068ddca3469bbcabd93dfcc56925
amin-tennah <amin.tennah@edu.esiee.fr>
2024-11-14 09:58:55 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+'''Code d'un palyndrome'''
 #### Fonction secondaire
 
 
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    """
+    gjr
+    """
+    p = p.lower()
+    accents = "àâäéèêëîïôöùûüç-.:?!,';_"
+    sans_accents = "aaaeeeeiioouuuc         "
+    table = str.maketrans(accents, sans_accents)
+    cleaned_string = p.translate(table).replace(" ", "")
+    return cleaned_string == cleaned_string[::-1]
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    le main
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
